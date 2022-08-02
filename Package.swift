6f622e491bff7bd07f7fb5fe84bee51ff3e7c8f7
Stanislav Kutil <skutil@acrobits.cz>
2022-08-02 12:49:40 +0200

    Package updated to 180324

--- Package.swift
+++ Package.swift
@@ -9,20 +9,18 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SoftphoneSwiftPackage-saas-local",
-            targets: ["SoftphoneSwiftPackage-saas-local"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            targets: ["Softphone", "Softphone_Swift"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "SoftphoneSwiftPackage-saas-local",
-            dependencies: []),
-        .testTarget(
-            name: "SoftphoneSwiftPackage-saas-localTests",
-            dependencies: ["SoftphoneSwiftPackage-saas-local"]),
+        .binaryTarget(
+            name: "Softphone",
+            url: "https://dist.acrobits.net/saas-swift-package-local/debug/Softphone.xcframework-180324.zip",
+            checksum: "7a36883a287339073fa5f12d5ebd26656ec1729f4fa07896b031ba89d8504015"),
+        .binaryTarget(
+            name: "Softphone_Swift",
+            url: "https://dist.acrobits.net/saas-swift-package-local/debug/Softphone_Swift.xcframework-180324.zip",
+            checksum: "2ea8dda4df843b02489b8d434ab32c1ee62af2a5c2a22845b089103c4e31f8e9")
     ]
 )
